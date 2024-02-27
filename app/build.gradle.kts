15d04715bc1cca2d355ccaef35229bfc9ed720af
1fexd <PGFvy85nC@protonmail.com>
2024-02-27 22:49:08 +0100

    feat: Bump version

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -11,8 +11,8 @@ android {
         applicationId = "fe.linksheet.compat"
         minSdk = 24
         targetSdk = 34
-        versionCode = 4
-        versionName = "0.0.4"
+        versionCode = 5
+        versionName = "0.0.5"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         vectorDrawables {
